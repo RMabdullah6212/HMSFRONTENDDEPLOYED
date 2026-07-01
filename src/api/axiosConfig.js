799cf4bd18bb79c72e13e793a689fedd2112ff21
RMAbdullah6212 <abdullahaslam6212@gmail.com>
2026-07-01 16:42:34 +0500

    commit

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ||
-  "https://hospital-management-system-backend-blond.vercel.app/api/v1";
+console.log("API URL:", import.meta.env.VITE_API_BASE_URL);
 
 const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
